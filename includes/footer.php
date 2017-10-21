c10ce09363d959ae615fd7f510369a65a8b8d1e9
DaninhaC <daninha.conceicao1980@gmail.com>
2017-10-21 00:14:24 -0200

    1ªpasta

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div id="footer" style="background-color:#F7F7F7;font-size:17px;color:black" width="980">
+   <!-- Para criação do BD , utilizar o arquivo bd_solutions.sql-->
+         <p align="center"><b>Av: Dias Viêira, 556 - Conj. 01 - 12º Andar - São Paulo.
+                              Fone: + 55 (11)3333-9000 contato@solutions.com.br</b></p>
+
+     <div style="color:black" align="center">©2015 All Rights Reserved  •  Design by Daniele da Conceição 4º Ciclo Noturno.</div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<div id="header" >
+  <br>
+  <!-- Para criação do BD , utilizar o arquivo bd_solutions.sql-->
+     <div class="menu">
+             <a href="index.php">Home</a> <a href="institucional.php">Quem Somos</a>
+             <a href="prod_serv.php">Produtos & Serviços</a>
+             <a href="noticias.php">Notícias</a> <a href="contato.php">Contato</a> <a href="form.php">Cadastro</a>
+             <a href="consulta.php">Consulta</a>
+     </div> <!-- menu -->
+   <div id="logo" >
+
+     <p id="nome">SOLUTIONS <span class="y">TECNOLOGIAS AMBIENTAIS</span></a><span class="y"></span></p>
+   </div> <!-- logo -->
+
+</div> <!-- header-->
