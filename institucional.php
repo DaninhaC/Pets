3d39cdfd5b85b54412bb5d7803eaa8e4418b84e6
Daniele da Conceição <daninha.conceicao1980@gmail.com>
2018-04-14 20:41:09 -0300

    pg institucional add

---
+++ institucional.php
@@ -0,0 +1,46 @@
+﻿<!--
+Tarefa 0303_Daniele da Conceição
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SOLUTIONS CONSULTORIA AMBIENTAL</title>
+            
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        
+    </head>
+    <body bgcolor="#5dcaf1">
+        
+     <table bgcolor="ffffff"align="center" border="0" width="800" height="680">
+          <tr id="nome"><td colspan="2">SOLUTIONS <span class="y">CONSULTORIA AMBIENTAL</span></a><span class="y"></span></td>
+          </tr>
+          <tr height="20" bgcolor="#5dcaf1" id="a"><td  colspan="2" align="center"><a href="index.html">Home</a> | <a href="institucional.html">Institucional</a> |
+             <a href="noticias.html">Notícias</a> | <a href="prod_serv.html">Serviços</a> | <a href="contato.html">Contato</a></td>
+          </tr>
+           <tr><td height="10">&nbsp;</td><td>&nbsp;</td></tr>
+           <tr><td width="330" height="150"class="a" align="center"><b> MISSÃO</b><br/><br/>
+                   A missão da empresa é atuar com excelência em consultoria ambiental e nas 
+                   demais áreas de atuação, desenvolvendo para o mercado nacional soluções inovadoras
+                   capazes de promover o crescimento econômico com respeito ao meio ambiente, entendendo
+                   o homem como ator deste contexto.</td><td align="center"><img src="images/sol.jpg" width="300" height="185" /></td>
+           </tr>
+         <tr><td align="center"width="330" height="150"><img src="images/araras.jpg" width="275" height="183" /></td>
+              <td  class="a" align="center"><b>VALORES</b><br/><br/>
+                  Coexistência das tecnologias e de um ambiente saudável;<br/>
+                  Harmonizar o progresso humano e tecnológico;<br/>
+                  Alinhar questões econômicas sem sobreposição às ambientais.<br/>
+                 </td>
+          </tr>
+          <tr><td colspan="2" align="center"><img src="images/eiarima.jpg" width="150" height="90" />&nbsp;
+              <img src="images/ibama.jpg" width="200" height="120" /></td>
+          </tr>
+          <tr><td height="20" colspan="2" align="center"><b>Av: Dias Vieira, 556 - Conj. 01 - 12º Andar - São Paulo. 
+                      Fone: + 55 (11)3333-9000 contato@solutions.com.br</b></td></tr>
+          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+      </table> 
+        <br/>
+     <div style="color:whitesmoke" align="center">©2015 All Rights Reserved  •  Design by Daniele da Conceição.</div>  
+   </body>
+</html>
+
