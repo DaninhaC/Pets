5fedfdc5a1ff6f6873a32223b1c80bb7d4a473bb
Daniele da Conceição <daninha.conceicao1980@gmail.com>
2018-04-14 20:10:29 -0300

    add pg contato

---
+++ contato.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SOLUTIONS CONSULTORIA AMBIENTAL</title>
+         
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        
+    </head>
+    <body bgcolor="#5dcaf1">
+        
+     <table bgcolor="ffffff"align="center" border="1" width="800" height="680">
+          <tr id="nome"><td colspan="2">SOLUTIONS <span class="y">CONSULTORIA AMBIENTAL</span></a><span class="y"></span></td>
+          </tr>
+          <tr height="20" bgcolor="#5dcaf1" id="a"><td  colspan="2" align="center"><a href="index.html">Home</a> | <a href="institucional.html">Institucional</a> |
+             <a href="noticias.html">Notícias</a> | <a href="prod_serv.html">Serviços</a> | <a href="contato.html">Contato</a></td>
+          </tr>
+           <tr><td height="20">&nbsp;</td><td>&nbsp;</td></tr>
+           <tr><td class="e" colspan="2"align="center"><label><b>Fale Conosco</b></label></td>
+           </tr> 
+           <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+          <tr><td><table><tr><td><img src="images/tartaruga.jpg" width="281" height="180"/></td></tr></table></td>
+              <td><table class="f">
+                      <form action="contato.html">
+                <tr><td><label><b>Nome</b></label></td><td><input type="text" name="txtnm"size="40"/></td></tr>
+                <tr><td><label><b>Telefone</b></label></td><td><input type="text" name="ddd"size="3"/>&nbsp;<input type="text" name="tel"/></td></tr>
+                <tr><td><label><b>Assunto</b></label></td><td><input type="text" name="txtass"size="40"/></td></tr>
+                <tr><td><label><b>E-mail</b></label></td><td><input type="email" name="email"size="40"/></td></tr>
+                <tr><td><label><b>Mensagem</b></label></td><td><textarea name="Mensagem" rows="6" cols="43" maxlenght="250"></textarea></td></tr>
+            
+                <tr><td colspan="2" align="center"><input type="submit" name="bt1" value="Enviar"> &nbsp;&nbsp;&nbsp;
+                <input type="reset" name="bt2" value="Limpar"></td></tr>
+                     </form> 
+            </table> </td>
+          </tr>
+          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+          <tr><td align="center" colspan="2" width="800" height="150"><img src="images/cidade_arvore.jpg"width="650" height="160"/></td></tr>
+          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+          </tr><tr><td height="20" colspan="2" align="center"><b>Av: Dias Vieira, 556 - Conj. 01 - 12º Andar - São Paulo.
+                      Fone: + 55 (11)3333-9000 contato@solutions.com.br</b></td></tr>
+          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+     </table>
+       
+        <br/>
+     <div style="color:whitesmoke" align="center">@2015 All Rights Reserved   Design by Daniele da Conceição.</div>
+   </body>
+</html>
+
