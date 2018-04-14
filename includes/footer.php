b1e5badbdc608ebc3e604ebd0ff2f6b80970b1fd
Daniele da Conceição <daninha.conceicao1980@gmail.com>
2018-04-14 20:57:44 -0300

    footer adicionado

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<!-- comentario-->
+<div id="footer" style="background-color:#F7F7F7">
+         <p align="center"><b>Av: Dias Vieira, 556 - Conj. 01 - 12º Andar - São Paulo.
+                              Fone: + 55 (11)3333-9000 contato@solutions.com.br</b></p>
+
+     <div style="color:black" align="center">©2015 All Rights Reserved  •  Design by Daniele da Conceição.</div>
+</div>
