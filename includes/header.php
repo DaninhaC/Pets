a9f291aaab22adb68cd6dce1160b115933f31022
Daniele da Conceição <daninha.conceicao1980@gmail.com>
2018-04-14 20:55:00 -0300

    ad o header ao branch

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<div id="header" >
+  <br/>
+     <div class="menu">
+             <a href="index.php">Home</a> <a href="institucional.php">Sobre</a>
+             <a href="prod_serv.php">Produtos & Serviços</a>
+             <a href="noticias.php">Notícias</a> <a href="contato.php">Contato</a>
+     </div> <!-- menu -->
+   <div id="logo" >
+
+     <p id="nome">SOLUTIONS <span class="y">TECNOLOGIAS AMBIENTAIS</span></a><span class="y"></span></p>
+   </div> <!-- logo -->
+
+</div> <!-- header-->
